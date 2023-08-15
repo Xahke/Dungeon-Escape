7adabce9ccdca979535b68157a466bc369c2c2e2
Xahke <myrear.power@hotmail.com>
2023-08-15 14:18:01 +0300

    Skeleton AI
    Skeleton AI Updated.
    Skeleton Attack Added.
    When Skeleton Attackde Player Flip Sprite Problem Solved.

--- Assets/Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Assets/Scripts/Enemy/Enemy.cs
@@ -11,28 +11,31 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField]
     protected float speed;
     [SerializeField]
-    protected Transform pointA,pointB;
+    protected Transform pointA, pointB;
     protected Vector3 currentTarget;
     protected Animator anim;
     protected SpriteRenderer spriteRenderer;
     protected bool isHit;
+    protected GameObject Player;
 
     public virtual void Init()
     {
         anim = GetComponentInChildren<Animator>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        Player = GameObject.Find("Player");
     }
     public virtual void Start()
     {
-        Init(); 
+        Init();
     }
     public virtual void Update()
     {
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")&&anim.GetBool("InCombat")==false)
         {
             return;
         }
         Movement();
+
     }
 
     public virtual void Movement()
@@ -56,12 +59,30 @@ public abstract class Enemy : MonoBehaviour
             anim.SetTrigger("Idle");
             currentTarget = pointA.position;
         }
-        if (isHit==false)
+        if (isHit == false)
         {
             transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
         }
+        float distance = Vector3.Distance(transform.localPosition, Player.transform.localPosition);
+        if (distance>5.0f)
+        {
+            isHit = false;
+            anim.SetBool("InCombat", false);
+        }
+        Vector3 side = Player.transform.position - transform.position;
+        if (anim.GetBool("InCombat")==true)
+        {
+            if (side.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+        
         
     }
 
- 
 }
